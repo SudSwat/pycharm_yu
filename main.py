f957973fea06cc1e704e7bd34f897a66e1516b52
Sudhakar Shreya <sudhakarjnv@gmail.com>
2024-10-08 11:19:37 +0530

    done_office

--- main.py
+++ main.py
@@ -19,7 +19,15 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         # print(drink)
-        coffee_maker.is_resource_sufficient(drink)
+        if coffee_maker.is_resource_sufficient(drink):
+            if money_machine.make_payment(drink.cost):
+                coffee_maker.make_coffee(drink)
+        else:
+            print("Out of Resource")
+
+
+
+
 
 
 
